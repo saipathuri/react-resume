118ecdc33af14e0ba966872ee9f2b87679443321
Sai Pathuri <saipathuri@gmail.com>
2018-08-13 16:00:52 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, HashRouter } from "react-router-dom";
+import About from "./components/about";
+import Experience from "./components/experience";
+import Education from "./components/education";
+import Navbar from "./components/navbar";
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <Navbar />
+          <div className="container">
+            <Route exact path="/" component={About} />
+            <Route path="/experience" component={Experience} />
+            <Route path="/education" component={Education} />
+          </div>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg sticky-top">
+      <span className="navbar-brand mb-0 h1">Sai Pathuri</span>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <NavLink exact className="nav-link" to="/">
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/experience">
+              Experience
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/education">
+              Education
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
