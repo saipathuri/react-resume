0acc3023bd2d2d8810b50b15085cf462d251e110
Sai Pathuri <saipathuri@gmail.com>
2018-08-13 19:24:22 -0500

    finished education page

---
+++ src/components/educationItem.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const EducationItem = props => {
+  return (
+    <div className="row py-3">
+      <div className="col-lg-10 col-md-9">
+        <div className="mr-auto">
+          <h2>{props.schoolName}</h2>
+          <h5 className="text-muted">{props.degree}</h5>
+          <ul className="list-group d-inline-flex">
+            {props.descriptions.map(item => {
+              return (
+                <li className="list-group-item">
+                  <strong>{item.topic}</strong>: {item.description}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="col-lg-2 col-md-3">
+        <div className="text-md-right">
+          <span className="text-secondary">{props.date}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EducationItem;
--- src/components/pages/education.jsx
+++ src/components/pages/education.jsx
@@ -1,16 +1,52 @@
 import React, { Component } from "react";
+import EducationItem from "../educationItem";
 
 class Education extends Component {
-  state = {};
+  state = {
+    items: [
+      {
+        schoolName: "University of Texas at Dallas",
+        degree: "Bachelor of Science",
+        descriptions: [
+          {
+            topic: "GPA",
+            description: 3.94
+          },
+          {
+            topic: "Major",
+            description: "Computer Engineering"
+          }
+        ],
+        date: "Aug. 2015 - May 2019 (Expected)"
+      },
+      {
+        schoolName: "Westwood High School",
+        degree: "Diploma (Professional Studies Program)",
+        descriptions: [
+          {
+            topic: "GPA",
+            description: 4.95
+          }
+        ],
+        date: "Oct. 2017 - Dec. 2017"
+      }
+    ]
+  };
   render() {
     return (
-      <div className="row align-items-center fill">
+      <div className="row align-items-start fill my-3">
         <div className="col-lg">
-          <h2>GOT QUESTIONS?</h2>
-          <p>
-            The easiest thing to do is post on our{" "}
-            <a href="http://forum.kirupa.com">forums</a>.
-          </p>
+          <h1 className="display-4">Education</h1>
+          {this.state.items.map(item => {
+            return (
+              <EducationItem
+                schoolName={item.schoolName}
+                degree={item.degree}
+                descriptions={item.descriptions}
+                date={item.date}
+              />
+            );
+          })}
         </div>
       </div>
     );
